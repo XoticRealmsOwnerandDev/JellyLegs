54529cf6184ee1996a180ce26d5c1cdd0e7f7fd3
TheDiamondYT1 <luke.wyatt21@gmail.com>
2017-03-11 11:13:51 +0000

    Change namespace, register command using commandmap

---
+++ src/TheDiamondYT/JellyLegs/JellyLegsCommand.php
@@ -0,0 +1,103 @@
+<?php
+/*
+ *       _      _ _       _                    
+ *      | |    | | |     | |                   
+ *      | | ___| | |_   _| |     ___  __ _ ___ 
+ *  _   | |/ _ \ | | | | | |    / _ \/ _` / __|
+ * | |__| |  __/ | | |_| | |___|  __/ (_| \__ \
+ *  \____/ \___|_|_|\__, |______\___|\__, |___/
+ *                   __/ |            __/ |    
+ *                  |___/            |___/  
+ *
+ *
+ * By TheDiamondYT
+ */
+namespace TheDiamondYT\JellyLegs;
+
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\command\PluginIdentifiableCommand;
+use pocketmine\Player;
+
+use pocketmine\utils\TextFormat as TF;
+
+class JellyLegsCommand extends Command implements PluginIdentifiableCommand {
+	
+	private $plugin;
+	
+	public function __construct(Main $plugin) {
+	    parent::__construct("jellylegs", "Toggle fall damage for yourself or others.", "/jellylegs [player]", []);
+	    $this->setPermission("jellylegs.toggle.self");
+	    
+		$this->plugin = $plugin;
+	}
+	
+	public function execute(CommandSender $sender, $label, array $args) {
+	    if(empty($args)) {
+			if($sender instanceof Player) {
+				if(!$sender->hasPermission("jellylegs.*") || !$sender->hasPermission("jellylegs.toggle.self")) { 
+					$sender->sendMessage($this->plugin->prefix . TF::DARK_RED . "No permission.");
+					return true;
+				}
+				elseif(in_array($sender->getName(), $this->plugin->players)) {
+					unset($this->plugin->players[$sender->getName()]);
+					$sender->sendMessage($this->plugin->prefix . TF::RED . "You can now take fall damage.");
+					return true;
+				} 
+				else {
+					$this->plugin->players[$sender->getName()] = $sender->getName();
+					$sender->sendMessage($this->plugin->prefix . TF::GREEN . "You can no longer take fall damage.");
+					return true;
+				}
+			}
+			else {
+				$sender->sendMessage(TF::RED . "Usage: /jellylegs <player>");
+				return true;
+			}
+		}
+		if(count($args) === 1) {
+			if(strtolower($args[0]) === "reload") {
+				if(!$sender->hasPermission("jellylegs.*")) {
+					$sender->sendMessage($this->plugin->prefix . TF::DARK_RED . "No permission.");
+					return true;
+				}
+				$this->plugin->reloadConfig();
+				$sender->sendMessage($this->plugin->prefix . TF::GOLD . "Config has been reloaded!");
+				return true;
+			}
+			if(!$sender->hasPermission("jellylegs.*") || !$sender->hasPermission("jellylegs.toggle.others")) { 
+				$sender->sendMessage($this->plugin->prefix . TF::DARK_RED . "No permission.");
+				return true;
+			}
+
+			$target = $this->plugin->getServer()->getPlayer($args[0]);
+					
+			if(!$target === null) {
+				if(in_array($target->getName(), $this->plugin->players)) {
+					unset($this->plugin->players[$target->getName()]);
+					$sender->sendMessage($this->plugin->prefix . TF::RED . "Player '" . $target->getName() . "' can now take fall damage.");
+					$target->sendMessage($this->plugin->prefix . TF::RED . "You can now take fall damage.");
+					return true;
+				} 
+				else {
+					$this->plugin->players[$target->getName()] = $target->getName(); // TODO: Use players UUID instead?
+					$sender->sendMessage($this->plugin->prefix . TF::GREEN . "Player '" . $target->getName() . "' can no longer take fall damage.");
+					$target->sendMessage($this->plugin->prefix . TF::GREEN . "You can no longer take fall damage.");
+					return true;
+				} 
+			}
+			else {
+				$sender->sendMessage(TF::RED . "That player cannot be found.");
+				return true;
+			}
+		}
+		if(count($args) >= 2) {
+			$sender->sendMessage($sender instanceof Player ? TF::RED . "Usage: /jellylegs [player]" : TF::RED . "Usage: /jellylegs <player>");
+			return false;
+		}
+	}
+	
+	public function getPlugin(){
+		return $this->plugin;
+	}
+}
